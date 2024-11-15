862e9f0f3a68c47f376352304d17ea87a3fc1f25
EllianaRico24 <ellianarico24@gmail.com>
2024-11-14 20:39:23 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//When the corresponding button is pressed, will make Image A, B, or C visible
+function showA() {
+    document.getElementById("image-a").style.visibility = 'visible';
+}
+
+function showB() {
+    document.getElementById("image-b").style.visibility = 'visible';
+}
+
+function showC() {
+    document.getElementById("image-c").style.visibility = 'visible';
+}
+
+//When the corresponding button is pressed, whill hide Image A, B, or C
+function hideA() {
+    document.getElementById("image-a").style.visibility = 'hidden';
+}
+
+function hideB() {
+    document.getElementById("image-b").style.visibility = 'hidden';
+}
+
+function hideC() {
+    document.getElementById("image-c").style.visibility = 'hidden';
+}
